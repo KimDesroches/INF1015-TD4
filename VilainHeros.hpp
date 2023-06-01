9525ac739cf654bc4c4616dcf9782e205fbeb226
Kim Desroches <102703442+KimDesroches@users.noreply.github.com>
2023-06-01 15:09:41 -0400

    enlever espace

--- VilainHeros.hpp
+++ VilainHeros.hpp
@@ -6,23 +6,23 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <string>
-#include "Personnage.hpp"
+#include "Heros.hpp"
+#include "Vilain.hpp"
 #include <vector>
 using namespace std;
 
 class VilainHeros : public Heros, public Vilain {
 public:
-	//TODO: constructeur
 	VilainHeros(Vilain v, Heros h) : Personnage(v.getNom() + "-" + h.getNom(), v.getParution() + "-" + h.getParution()),
-		Vilain(v.getNom(), v.getParution(), v.getObjectif()), Heros(h.getNom(), h.getParution(),h.getEnnemi(), h.getAllies()), missionSpeciale_(v.getObjectif() + "dans le monde de " + h.getParution()) {};
+		Vilain(v.getNom(), v.getParution(), v.getObjectif()), Heros(h.getNom(), h.getParution(),h.getEnnemi(), h.getAllies()), missionSpeciale_(v.getObjectif() + " dans le monde de " + h.getParution()) {};
 
 	void afficher(ostream& os, char couleur = 'w') {
 		Vilain::afficher(os, couleur);
 		changerCouleur(os, couleur);
-		os << "Ennemi : " << Heros::getEnnemi() << "\nAlliés : \n";
+		os << "Ennemi : " << Heros::getEnnemi() << "\nAlliÃ©s : \n";
 		for (auto&& elem : Heros::getAllies())
 			os << "\t" << elem << endl;
-		os << "Mission spéciale : " << missionSpeciale_ << endl;
+		os << "Mission spÃ©ciale : " << missionSpeciale_ << endl;
 		changerCouleur(os);
 	}
 
